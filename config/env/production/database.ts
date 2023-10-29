f6d86c9ffb86c32f0e80e9b160b6f21b47fe3417
Gonzalo <gonzalosalvadorcorvalan@gmail.com>
2023-10-29 13:34:32 -0300

    added MySQL connection

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,16 +1,13 @@
 export default ({ env }) => ({
 	connection: {
-		client: "postgres",
+		client: "mysql",
 		connection: {
 			host: env("DATABASE_HOST", "127.0.0.1"),
-			port: env.int("DATABASE_PORT", 5432),
+			port: env.int("DATABASE_PORT", 3306),
 			database: env("DATABASE_NAME", "strapi"),
 			user: env("DATABASE_USERNAME", "strapi"),
 			password: env("DATABASE_PASSWORD", "strapi"),
-			schema: env("DATABASE_SCHEMA", "public"), // Not required
-			ssl: {
-				// rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-			},
+			ssl: {},
 		},
 		debug: false,
 	},
